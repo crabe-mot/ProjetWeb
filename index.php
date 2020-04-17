20440181a1c61a173f354797bddf79ca335d3521
Marcowflash <36543052+Marcowflash@users.noreply.github.com>
2020-04-17 12:28:44 +0200

    Connection ça fonctionne ?

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <?php
 
+if(!empty($_POST['username']) && !empty($_POST['password'])) {
+	  $database = new DBConnect;
+	  $sql = "select firstname, lastname from customers where email='".$_POST['username']."' and password='".$_POST['password']."';";
+	  $_SESSION['email'] = $_POST['username'];
+	  $sth = $database->getInstance()->prepare($sql);
+	  $sth->execute();
+	  $result = $sth->fetchColumn();
+	  
+	  if(empty($result))
+		  echo 'erreur mot de passe';
+
+	  $_SESSION['firstname'] = $result;
+	  $result = $sth->fetchColumn();
+	  $_SESSION['lastname'] = $result;
+  }
+
   if (isset($_GET['controller']) && isset($_GET['action'])){
    $controller= $_GET['controller'];
    $action=$_GET['action'];
