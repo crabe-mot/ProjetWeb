36586ab3a3617277e3d8902193289d2f1c1b6570
Raphaël Brogat <raphael.brogat@insa-cvl.fr>
2020-04-17 18:45:38 +0200

    correction

--- views/products/show.php
+++ views/products/show.php
@@ -2,8 +2,7 @@
     <a  class="product-image" href="static/images/<?= $product->image ?>">
         <img class="standard-size" src="static/images/<?= $product->image ?>" alt="<?= $product->name ?>" title="<?= $product->name ?>">
     </a>
-    <h2><?= $product->name ?></h3>
-        <p><?= $product->description ?></p>
-        <span>$ <?= $product->price ?></span>
-        <span class="see_details">See products details...</span>
+    <h2 class="product-name"><?= $product->name ?></h2>
+    <div class="product-desc"><?= $product->description ?></div>
+    <div class="product-price">$ <?= $product->price ?></div>
 </div>
