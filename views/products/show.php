db594fd0205bf905b512974c640e2955a5a27061
Raphaël Brogat <raphael.brogat@insa-cvl.fr>
2020-04-23 21:45:20 +0200

    last stuff

--- views/products/show.php
+++ views/products/show.php
@@ -1,8 +1,13 @@
 <div class="products_info">
-    <a  class="product-image" href="static/images/<?= $product->image ?>">
-        <img class="standard-size" src="static/images/<?= $product->image ?>" alt="<?= $product->name ?>" title="<?= $product->name ?>">
-    </a>
-    <h2 class="product-name"><?= $product->name ?></h2>
-    <div class="product-desc"><?= $product->description ?></div>
-    <div class="product-price">$ <?= $product->price ?></div>
+    <div class="column">
+        <a  class="product-image" href="static/images/<?= $product->image ?>">
+            <img class="standard-size" src="static/images/<?= $product->image ?>" alt="<?= $product->name ?>" title="<?= $product->name ?>">
+        </a>
+    </div>
+    <div class="column">
+        <h2 class="product-name"><?= $product->name ?></h2>
+        <div class="product-desc"><?= $product->description ?></div>
+        <div class="product-price">$ <?= $product->price ?></div>
+    </div>
+
 </div>
